b675953fcb2d48a098b09499719ba69f7fecb45e
Petrova <petrova01269@gmail.com>
2022-11-06 13:40:34 +0400

    initial

---
+++ src/test/java/ru/sseu/envy/EnvyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sseu.envy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
